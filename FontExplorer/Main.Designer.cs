7caa79ed6e672f037d07ab9077d5536120ae2456
Hans Isbrücker <hisbrucker@gmail.com>
2024-08-13 13:58:57 +0200

    Add project files.

---
+++ FontExplorer/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FontExplorer
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            toolStripContainer1 = new ToolStripContainer();
+            fontContainer = new FlowLayoutPanel();
+            toolStrip1 = new ToolStrip();
+            tbText = new ToolStripTextBox();
+            btnShow = new ToolStripButton();
+            toolTip1 = new ToolTip(components);
+            toolStripContainer1.ContentPanel.SuspendLayout();
+            toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            toolStripContainer1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            toolStripContainer1.ContentPanel.Controls.Add(fontContainer);
+            toolStripContainer1.ContentPanel.Size = new Size(800, 425);
+            toolStripContainer1.Dock = DockStyle.Fill;
+            toolStripContainer1.Location = new Point(0, 0);
+            toolStripContainer1.Name = "toolStripContainer1";
+            toolStripContainer1.Size = new Size(800, 450);
+            toolStripContainer1.TabIndex = 0;
+            toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            toolStripContainer1.TopToolStripPanel.Controls.Add(toolStrip1);
+            // 
+            // fontContainer
+            // 
+            fontContainer.AutoScroll = true;
+            fontContainer.Dock = DockStyle.Fill;
+            fontContainer.Location = new Point(0, 0);
+            fontContainer.Name = "fontContainer";
+            fontContainer.Size = new Size(800, 425);
+            fontContainer.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Dock = DockStyle.None;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { tbText, btnShow });
+            toolStrip1.Location = new Point(3, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(154, 25);
+            toolStrip1.TabIndex = 0;
+            // 
+            // tbText
+            // 
+            tbText.Name = "tbText";
+            tbText.Size = new Size(100, 25);
+            // 
+            // btnShow
+            // 
+            btnShow.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnShow.Image = (Image)resources.GetObject("btnShow.Image");
+            btnShow.ImageTransparentColor = Color.Magenta;
+            btnShow.Name = "btnShow";
+            btnShow.Size = new Size(40, 22);
+            btnShow.Text = "Show";
+            btnShow.ToolTipText = "Show the text in different fonts";
+            btnShow.Click += btnShow_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(toolStripContainer1);
+            Name = "Main";
+            Text = "Font Explorer";
+            toolStripContainer1.ContentPanel.ResumeLayout(false);
+            toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            toolStripContainer1.TopToolStripPanel.PerformLayout();
+            toolStripContainer1.ResumeLayout(false);
+            toolStripContainer1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ToolStripContainer toolStripContainer1;
+        private ToolStrip toolStrip1;
+        private ToolStripTextBox tbText;
+        private ToolStripButton btnShow;
+        private FlowLayoutPanel fontContainer;
+        private ToolTip toolTip1;
+    }
+}
---
+++ FontExplorer/Main.cs
@@ -0,0 +1,55 @@
+using System.Drawing.Design;
+using System.Drawing.Text;
+
+namespace FontExplorer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            ShowFonts();
+        }
+
+        private void ShowFonts()
+        {
+            string text = tbText.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show(this, "Empty text", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // get all the fonts
+            Padding padding = new Padding(10);
+            using (InstalledFontCollection installedFontCollection = new())
+            {
+                var families = installedFontCollection.Families;
+                foreach (var family in families.Where(f => f.IsStyleAvailable(FontStyle.Bold)))
+                {
+                    Label label = new()
+                    {
+                        Text = text,
+                        Tag = family,
+                        Font = new Font(family, 16.0f),
+                        Margin = padding
+                    };
+
+                    label.Click += (sender, e) =>
+                    {
+                        fontContainer.Controls.Remove(label);
+                    };
+
+                    toolTip1.SetToolTip(label, family.Name);
+                    fontContainer.Controls.Add(label);
+                }
+            }
+
+
+        }
+    }
+}
